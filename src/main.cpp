d34f4b9a06799743cc0aae88d221a837171807cb
Samuel Kaiponen <samuel.kaiponen@gmail.com>
2019-03-01 11:10:21 +0200

    Added a loop that asks for more paths to be included in the deduplication

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
@@ -11,62 +12,37 @@ using std::unordered_map;
 using std::vector;
 namespace fs = std::filesystem;
 
-int main(int argc, char *argv[])
+hashwrapper *myWrapper;
+unordered_map<string, vector<string>> dedup_table;
+
+void gather_hashes(fs::path path)
 {
-
-    if (argc < 2)
-    {
-        cout << "Usage: main path\n";
-        return 1;
-    }
-
-    fs::path p(argv[1]);
-    hashwrapper *myWrapper = new md5wrapper();
     try
     {
-        // cout << myWrapper->getHashFromString("Hello world") << endl;
-        // cout << myWrapper->getHashFromFile("/etc/group") << endl;
-        cout << fs::current_path() << endl;
-    }
-    catch (hlException &e)
-    {
-        cout << e.error_message() << endl;
-    }
-
-    unordered_map<string, vector<string>> dedup_table;
-
-    try
-    {
-        if (exists(p))
+        if (fs::exists(path))
         {
-            if (is_regular_file(p))
+            if (fs::is_regular_file(path))
             {
-                cout << p << " size is " << file_size(p) << '\n';
                 try
                 {
-                    string hash = myWrapper->getHashFromFile(p);
-                    dedup_table[hash].push_back(fs::canonical(p));
-                    cout << "   " << hash << endl;
+                    string hash = myWrapper->getHashFromFile(path);
+                    dedup_table[hash].push_back(fs::canonical(path));
                 }
                 catch (hlException &e)
                 {
                     cout << e.error_message() << endl;
                 }
             }
-            else if (is_directory(p))
+            else if (fs::is_directory(path))
             {
-                cout << p << " is a directory containing:\n";
-
-                for (const fs::directory_entry &x : fs::directory_iterator(p))
+                for (const fs::directory_entry &x : fs::directory_iterator(path))
                 {
-                    cout << "    " << x.path();
                     if (fs::is_regular_file(x))
                     {
                         try
                         {
                             string hash = myWrapper->getHashFromFile(x.path());
                             dedup_table[hash].push_back(fs::canonical(x.path()));
-                            cout << "   " << hash << endl;
                         }
                         catch (hlException &e)
                         {
@@ -81,27 +57,62 @@ int main(int argc, char *argv[])
             }
             else
             {
-                cout << p << " exists, but is not a regular file or directory\n";
+                cout << path << " exists, but is not a regular file or directory\n";
             }
         }
         else
         {
-            cout << p << " does not exist\n";
+            cout << path << " does not exist\n";
         }
     }
     catch (const fs::filesystem_error &ex)
     {
         cout << ex.what() << '\n';
     }
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2)
+    {
+        cout << "Usage: dedup PATH\n";
+        return 1;
+    }
+
+    cout << argv[1] << endl;
+    fs::path path(argv[1]);
+    myWrapper = new md5wrapper();
+
+    gather_hashes(path);
+
+    while (true)
+    {
+        cout << "Input another path to be included in "
+        "the deduplication or finish with N" << endl;
+
+        string input;
+        getline(cin, input);
+        if (input == "N")
+        {
+            break;
+        }
+
+        gather_hashes(fs::path(input));
+    }
+
+    cout << endl
+         << "Contents of the deduplication table:\n"
+         << endl;
 
     for (const auto &pair : dedup_table)
     {
-        cout << "Key:[" << pair.first << "] Values:[ ";
+        cout << "Key: " << pair.first << "\nValues:\n";
         for (const auto &path : pair.second)
         {
-            cout << path << " ";
+            cout << path << endl;
         }
-        cout << "]" << endl;
+        cout << endl;
     }
 
     delete myWrapper;
