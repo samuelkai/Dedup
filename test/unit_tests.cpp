cc88ad1e64e78ec722dec6558b95f35d9af90ef6
Samuel Kaiponen <samuel.kaiponen@gmail.com>
2019-09-06 13:30:13 +0300

    Tweak build system

---
+++ test/unit_tests.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "catch2/catch.hpp"
+#include "gather_hashes.h"
+
+TEST_CASE( "simple" )
+{
+    std::unordered_map<uint64_t, std::vector<std::string>> testmap;
+    gather_hashes("../../testi", testmap);
+    REQUIRE( testmap.size() == 4 );
+}
